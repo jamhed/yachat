c1451c88011f4fb19bb260a6dfaddbd156b367ce
Roman Galeev <roman@jamhed.tk>
2015-06-12 17:07:07 +0200

    single user online session, lobby

---
+++ cs/Online.cs
@@ -0,0 +1,17 @@
+define ["Nsend", "Cmon"], (Pi, Cmon) -> class Online extends Pi
+
+   attr: -> super.concat ["view"]
+
+   init: ->
+      @wait_ajax_done () => @query()
+   
+   draw: (userList) ->
+      @clear()
+      tmpl = @rt.source @a.view
+      @e.append tmpl {id: userInfo[0], display: Cmon.displayNameA(userInfo) } for userInfo in userList
+      @rt.pi @e
+
+   query: ->
+      @nsend ["user/online", Cmon.sid()], (status, userList) =>
+         @debug status, userList
+         @draw userList
