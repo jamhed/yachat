92ae27d09dbc17399d51b230f725bcb0a66d52b7
Roman Galeev <jamhedd@gmail.com>
2015-06-16 00:18:43 +0200

    cosmetic

--- apps/cmon/src/logger.erl
+++ apps/cmon/src/logger.erl
@@ -1,6 +1,5 @@
 -module(logger).
 -compile(export_all).
-
 -include_lib("cmon/include/config.hrl").
 
 get_app() ->
@@ -8,7 +7,6 @@ get_app() ->
       {ok, App} -> atom_to_list(App);
       _         -> "_"
    end.
-
 
 log_f(Module, Line, _String, Args, Fun) ->
    case List = ?CFG(log_modules, []) of
@@ -23,7 +21,6 @@ log_f(Module, Line, _String, Args, Fun) ->
 log(Module, Line, _String, Args, Fun) ->
    String = get_app() ++ "/" ++ atom_to_list(Module) ++ "." ++ integer_to_list(Line) ++ ": " ++ _String ++ "~n",
    error_logger:Fun(String, Args).
-
 
 info(Module, Line, String, Args)       -> log_f(Module, Line, String, Args, info_msg).
 warning(Module, Line, String, Args)    -> log_f(Module, Line, String, Args, warning_msg).
