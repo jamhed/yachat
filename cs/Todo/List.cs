87621e66b6576461d581d478904c7c4a4e81e070
Roman Galeev <roman@jamhed.tk>
2015-08-12 23:49:33 +0200

    design: inplace todo editor

--- cs/Todo/List.cs
+++ cs/Todo/List.cs
@@ -1,17 +1,29 @@
-define ["Nsend", "Cmon"], (Pi, Cmon) -> class TodoList extends Pi
+define ["Nsend", "Cmon", "pi/lib/jquery-ui"], (Pi, Cmon, UI) -> class TodoList extends Pi
 
-	attr: -> super.concat ["default"]
+	attr: -> super.concat ["default", "id"]
 
 	init: ->
+		super
+		@e.selectmenu
+			change: (ev, ui) => @onChange(ev, ui)
 		@query()
 
+	onChange: (ev, ui) ->
+		@e.attr "default", ui.item.value
+		@a.default = ui.item.value
+		value = if parseInt(ui.item.value) then parseInt(ui.item.value) else ui.item.value
+		@nsend ["todo/move_to", Cmon.sid(), parseInt(@a.id), value], => @query()
+
 	draw: (List) ->
+		@e.empty()
 		List.push id: "delete", name: "Delete"
 		List.push id: "keep", name: "Keep"
 		for T in List
 			do (T) =>
+				@debug @a.default, T.id
 				sel = if "#{T.id}" == @a.default then "selected" else ""
 				@e.append "<option #{sel} value=#{T.id}>#{T.name}"
+		@e.selectmenu("refresh")
 
 	query: ->
 		@nsend ["todo/list", Cmon.sid()], (List) => @draw(List)
