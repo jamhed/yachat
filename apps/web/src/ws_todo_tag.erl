f512003ae1a2523209d781743af1175b404e9ffc
Roman Galeev <roman@jamhed.tk>
2015-08-28 13:25:35 +0200

    feature: default todo/tag as attrs

--- apps/web/src/ws_todo_tag.erl
+++ apps/web/src/ws_todo_tag.erl
@@ -4,23 +4,18 @@
 -include_lib("web/include/db.hrl").
 
 %msg get list of todo tags
-msg(M = <<"todo/tags">>, [Uid, Sid]) ->
-	[M, db_session:get(Sid, tag)]
+msg(M = <<"todo/tags">>, [Uid]) ->
+	[M, db_todo:get_default_tag(Uid)]
 		++ [lists:usort([ Tag || #todo_tag{tag=Tag} <- db_todo:get_all_tags(Uid) ])];
 
 %msg set current tag
-msg(M = <<"todo/tag/set">>, [_Uid, Sid, Tag]) ->
-	db_session:set(Sid, tag, Tag),
-	[M, ok];
-
-%msg clear current tag
-msg(M = <<"todo/tag/clear">>, [_Uid, Sid]) ->
-	db_session:del(Sid, tag),
+msg(M = <<"todo/tag/set">>, [Uid, Tag]) ->
+	db_todo:set_default_tag(Uid, Tag),
 	[M, ok];
 
 %msg get current tag
-msg(M = <<"todo/tag/current">>, [_Uid, Sid]) ->
-	Tag = db_session:get(Sid, tag),
+msg(M = <<"todo/tag/current">>, [Uid]) ->
+	Tag = db_todo:get_default_tag(Uid),
 	[M, Tag];
 
 msg(_,_) -> skip.
